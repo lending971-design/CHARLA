e27c904b8571e0082c5070f89368662d1279ba08
lending971-design <lending971@gmail.com>
2026-04-01 15:16:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+const CONFIG = {
+
+  // 🔴 FOTO PROFIL (18+ LOGO)
+  profile: "https://i.imgur.com/YznES9G.jpeg",
+
+  // 🖼️ BACKGROUND SLIDER
+  backgrounds: [
+    "https://i.imgur.com/5nBqd6L.jpeg",
+    "https://i.imgur.com/YznES9G.jpeg"
+  ],
+
+  // ✨ TEXT
+  joinText: "Join.... ❤️",
+  bottomText: "ADULT ONLY 18+",
+
+  // 🔗 LINK BUTTON
+  links: [
+    {
+      name: "WhatsApp",
+      url: "https://wa.me/628xxxx",
+      icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png"
+    },
+    {
+  name: "Telegram",
+  url: "https://t.me/username_kamu",
+  icon: "https://cdn-icons-png.flaticon.com/512/2111/2111646.png"
+    }
+  ]
+
+};
